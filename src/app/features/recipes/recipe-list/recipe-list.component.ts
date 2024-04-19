d8f85a4f2eaea2c221406068b24a1b4a455bfc2c
dnanseldev <dnansel.dev@gmail.com>
2024-04-19 16:30:15 -0300

    Some layout structuring

--- src/app/features/recipes/recipe-list/recipe-list.component.ts
+++ src/app/features/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css',
 })
-export class RecipeListComponent {}
+export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is a simply a test', 'https://images.app.goo.gl/kpjXsBCHavy1MWxAA'),
+    new Recipe('A test Recipe', 'This is a simply a test', 'https://images.app.goo.gl/kpjXsBCHavy1MWxAA'),
+    new Recipe('A test Recipe', 'This is a simply a test', 'https://images.app.goo.gl/kpjXsBCHavy1MWxAA'),
+  ];
+}
