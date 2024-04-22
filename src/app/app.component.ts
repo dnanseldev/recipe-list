cda299f1a2b827aede432ae12a5ddf079c1955a3
dnanseldev <dnansel.dev@gmail.com>
2024-04-22 12:03:21 -0300

    Improvements in menu item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-list';
+
+  loadedFeature = 'recipe';
+
+  hdlNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
